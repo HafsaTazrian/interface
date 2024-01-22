931db349797c6bca99ffe463344d6e6dc3b15f6c
HafsaTazrian <hafsa.tazrian@gmail.com>
2024-01-22 15:32:08 +0600

    second commit

---
+++ interface.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+public interface Shape{
+    void draw();
+}
+int main()
+{
+    
+}
