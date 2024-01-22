61c3eaa03017b6bdbd6f6afe8ca420a04f22ca4c
HafsaTazrian <hafsa.tazrian@gmail.com>
2024-01-22 16:03:33 +0600

    last

--- interface.cpp
+++ interface.cpp
@@ -20,7 +20,55 @@ public:
         std::cout << "Inside Square::draw() method." << std::endl;
     }
 };
+class ShapeFactory {
+private:
+    static ShapeFactory* ins;
+    ShapeFactory() {}
+public:
+    static ShapeFactory* getInstance() {
+        if (ins == nullptr) {
+            ins = new Singleton();
+        }
+        return ins;
+    }
+    static void releaseInstance() {
+        if (ins != nullptr) {
+            delete ins;
+            ins = nullptr;
+        }
+    }
+
+    Shape* getShape(const std::string& shapeType) {
+        if (shapeType.empty()) {
+            return nullptr;
+        }
+
+        if (shapeType == "RECTANGLE") {
+            return new Rectangle();
+        } else if (shapeType == "SQUARE") {
+            return new Square();
+        }
+
+        return nullptr;
+    }
+};
+
+ShapeFactory* ShapeFactory::ins = nullptr;
 int main()
 {
+    ShapeFactory* x = ShapeFactory::getInstance();
+    Shape* shape1 = x.getShape("CIRCLE");
+    if (shape1 != nullptr) {
+        shape1->draw();
+        delete shape1;
+    }
+
+    Shape* shape2 = x.getShape("RECTANGLE");
+    if (shape2 != nullptr) {
+        shape2->draw();
+        delete shape2;
+    }
+
+    return 0;
     
 }
